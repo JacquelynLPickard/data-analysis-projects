23a294d0428629875d6a30f7fc937fa8e17e3026
O3XAI <o3xaiglobal@gmail.com>
2026-05-08 01:14:33 -0400

    Add Uber SQL queries notebook

--- uber-sql-analysis/queries.sql
+++ uber-sql-analysis/queries.sql
@@ -1,1 +1,3 @@
--- Uber SQL Analysis Queries
+-- List all tables in the database
+SELECT name FROM sqlite_master WHERE type='table';-- Query 1: Cities with most users
+SELECT city, COUNT(*) as total_users FROM users GROUP BY city ORDER BY total_users DESC;
